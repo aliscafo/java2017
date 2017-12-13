8fd2ca508a03b76d7db4e64a6316e2e767ac4790
Alina Erokhina <aliscafo@Alinas-MacBook-Pro.local>
2017-12-13 16:33:42 +0300

    Injector was added.

---
+++ Injector/src/test/java/ru/spbau/erokhina/cw2/testClasses/ClassWithOneClassDependency.java
@@ -0,0 +1,10 @@
+package ru.spbau.erokhina.cw2.testClasses;
+
+public class ClassWithOneClassDependency {
+
+    public final ClassWithoutDependencies dependency;
+
+    public ClassWithOneClassDependency(ClassWithoutDependencies dependency) {
+        this.dependency = dependency;
+    }
+}
